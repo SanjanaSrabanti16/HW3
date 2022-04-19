1f1251f33416313509a1d621a24f7f63799d5384
SanjanaSrabanti16 <ssraba2@uic.edu>
2022-04-19 00:12:58 -0500

    Updated Y values for origin state

--- JS.js
+++ JS.js
@@ -142,8 +142,8 @@
 
             // Because we don't want to have the device upside down
             // We constrain the x value to the range [-90,90]
-            if (x > 90) { x = 90 };
-            if (x < -90) { x = -90 };
+            // if (x > 90) { x = 90 };
+            // if (x < -90) { x = -90 };
 
             // To make computation easier we shift the range of
             // x and y to [0,180]
@@ -155,11 +155,11 @@
             // ball.style.left = (maxY * y / 180 - 10) + "px";
             // ball.style.top = (maxX * x / 180 - 10) + "px";
 
-            if (x > 45 && x < 90) {            	
+            if (x > 0 && x < 180) {            	
             	//undo();
             	$('#btnUndo:not(.btn-dis)').click();
             	flag = false;
-            } else if(x > -90 && x < -45) {
+            } else if(x > -180 && x < -90) {
             	//redo();
             	$('#btnRedo:not(.btn-dis)').click();
             	flag = false;
