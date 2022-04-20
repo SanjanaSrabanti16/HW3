f154fdb0dcadf564869878d55b1d07a80b977fb5
SanjanaSrabanti16 <ssraba2@uic.edu>
2022-04-20 00:56:16 -0500

    Fixed undo/redo

--- JS.js
+++ JS.js
@@ -8,113 +8,115 @@
         // var maxY = garden.clientHeight - ball.clientHeight;
 
         const inputBox = document.getElementById('inputBox');
-        const btnAdd = document.getElementById('btnAdd');
-        const btnUndo = document.getElementById('btnUndo');
-        const btnRedo = document.getElementById('btnRedo');
-        const displayBox = document.getElementById('displayBox');
-        const countBox = document.getElementById('countBox');
+        // const btnAdd = document.getElementById('btnAdd');
+        // const btnUndo = document.getElementById('btnUndo');
+        // const btnRedo = document.getElementById('btnRedo');
+        // const displayBox = document.getElementById('displayBox');
+        // const countBox = document.getElementById('countBox');
         const output = document.querySelector('.permissionClass');
 
-        const txtSave = []; // array to save values.
-        const txtDisplay = []; // array to display values.
-        let txtCount = 0; // counts length of current values.
-        let flag = true;
-        showAll = () => {
-            displayBox.innerHTML = txtDisplay;
-            countBox.innerHTML = txtCount;
+        // const txtSave = []; // array to save values.
+        // const txtDisplay = []; // array to display values.
+        // let txtCount = 0; // counts length of current values.
+        // let flag = true;
+        // showAll = () => {
+        //     displayBox.innerHTML = txtDisplay;
+        //     countBox.innerHTML = txtCount;
 
-            displayBox.innerHTML = txtDisplay.join(', ');
+        //     displayBox.innerHTML = txtDisplay.join(', ');
 
-        }; // displays all arrays and values.
+        // }; // displays all arrays and values.
 
-        showAll();
+        // showAll();
 
-        btnSwitch = () => {
-            if (txtCount <= 0) {
-                btnUndo.disabled = true;
-                btnUndo.classList.add('btn-dis');
-            } else {
-                btnUndo.disabled = false;
-                btnUndo.classList.remove('btn-dis');
-            }
+        // btnSwitch = () => {
+        //     // if (txtCount <= 0) {
+        //     //     btnUndo.disabled = true;
+        //     //     btnUndo.classList.add('btn-dis');
+        //     // } else {
+        //     //     btnUndo.disabled = false;
+        //     //     btnUndo.classList.remove('btn-dis');
+        //     // }
 
-            if (txtDisplay.length == txtSave.length) {
-                btnRedo.disabled = true;
-                btnRedo.classList.add('btn-dis');
-            } else {
-                btnRedo.disabled = false;
-                btnRedo.classList.remove('btn-dis');
-            };
+        //     // if (txtDisplay.length == txtSave.length) {
+        //     //     btnRedo.disabled = true;
+        //     //     btnRedo.classList.add('btn-dis');
+        //     // } else {
+        //     //     btnRedo.disabled = false;
+        //     //     btnRedo.classList.remove('btn-dis');
+        //     // };
 
-        }; // disables the buttons if 'undos' or 'redos' are not avalible and adds a class for styling and creating visual user feeback.
+        // }; // disables the buttons if 'undos' or 'redos' are not avalible and adds a class for styling and creating visual user feeback.
 
-        btnSwitch()
+        // btnSwitch()
 
-        add = () => {
-            const index = txtDisplay.length;
-            const saveCount = txtSave.length;
-            txtSave.splice(index, saveCount); // this takes length of both arrays and tells the saved array to go to the index equal to the current length of the displayed array, and then to remove all items after that index.
-            // ^
-            // (it's actually only removing the number of items equivalent to the current length of the saved array, but that number will always be higher then the number of proceeding items from the specified index, so it will always remove all the proceeding items no matter the length).
-            // ^
-            // this functionality is used for when you 'add' an item after 'undoing' some items, it deletes all the 'redos' you had avalible, and starts a new saved array, while still keeping any 'undos' you have not used avalible to you.
+        // add = () => {
+        //     const index = txtDisplay.length;
+        //     const saveCount = txtSave.length;
+        //     txtSave.splice(index, saveCount); // this takes length of both arrays and tells the saved array to go to the index equal to the current length of the displayed array, and then to remove all items after that index.
+        //     // ^
+        //     // (it's actually only removing the number of items equivalent to the current length of the saved array, but that number will always be higher then the number of proceeding items from the specified index, so it will always remove all the proceeding items no matter the length).
+        //     // ^
+        //     // this functionality is used for when you 'add' an item after 'undoing' some items, it deletes all the 'redos' you had avalible, and starts a new saved array, while still keeping any 'undos' you have not used avalible to you.
 
-            txtDisplay.push(inputBox.value); // takes whatever was entered in the input and adds it too the displayed array.
-            txtSave.push(txtDisplay.slice(-1)); // takes the last value in the displayed array and adds it to the end of the saved array.
-        };
+        //     txtDisplay.push(inputBox.value); // takes whatever was entered in the input and adds it too the displayed array.
+        //     txtSave.push(txtDisplay.slice(-1)); // takes the last value in the displayed array and adds it to the end of the saved array.
+        // };
 
-        undo = () => {
-            txtDisplay.pop();
-            // removes the last item from ONLY the displayed array.
-        };
+        // // undo = () => {
+        // //     txtDisplay.pop();
+        // //     // removes the last item from ONLY the displayed array.
+        // // };
 
-        redo = () => {
-            const txtGetLength = txtDisplay.length;
-            const txtGetValue = txtSave[txtGetLength];
-            txtDisplay.push(txtGetValue);
-            // this finds the length of the displayed area and then tells the saved array to go to the index equivalent to the length of the displayed array and then pushes the value to the displayed array.
-        };
+        // // redo = () => {
+        // //     const txtGetLength = txtDisplay.length;
+        // //     const txtGetValue = txtSave[txtGetLength];
+        // //     txtDisplay.push(txtGetValue);
+        // //     // this finds the length of the displayed area and then tells the saved array to go to the index equivalent to the length of the displayed array and then pushes the value to the displayed array.
+        // // };
 
-        btnAdd.onclick = () => {
+        // btnAdd.onclick = () => {
 
-            if (inputBox.value != '') {
-                txtCount++;
-                add();
-                btnSwitch();
-                showAll(); // all functions described above.
-            } else {
-                const txtAlert = document.createElement('p');
-                displayBox.appendChild(txtAlert);
-                txtAlert.innerHTML = 'write some text please'; // this happens incase the user doesn't enter any text into the input but tryies to push 'add' or hit enter.
-            };
+        //     if (inputBox.value != '') {
+        //         txtCount++;
+        //         add();
+        //         btnSwitch();
+        //         showAll(); // all functions described above.
+        //     } else {
+        //         const txtAlert = document.createElement('p');
+        //         displayBox.appendChild(txtAlert);
+        //         txtAlert.innerHTML = 'write some text please'; // this happens incase the user doesn't enter any text into the input but tryies to push 'add' or hit enter.
+        //     };
 
-            inputBox.value = ''; // makes input empty after adding value.
-        };
+        //     inputBox.value = ''; // makes input empty after adding value.
+        // };
 
-        btnUndo.onclick = () => {
-            if (flag) {
-                txtCount--;
-                undo();
-                btnSwitch();
-                showAll(); // all functions described above.
-            }
-        };
+        // btnUndo.onclick = () => {
+        //     // if (flag) {
+        //     //     txtCount--;
+        //     //     undo();
+        //     //     btnSwitch();
+        //     //     showAll(); // all functions described above.
+        //     // }
+        //     undo();
+        // };
 
-        btnRedo.onclick = () => {
-            if (flag) {
-                txtCount++;
-                redo();
-                btnSwitch();
-                showAll(); // all functions described above.
-            }
-        };
+        // btnRedo.onclick = () => {
+        //     // if (flag) {
+        //     //     txtCount++;
+        //     //     redo();
+        //     //     btnSwitch();
+        //     //     showAll(); // all functions described above.
+        //     // }
+        //     redo();
+        // };
 
-        inputBox.addEventListener("keyup", event => {
-            // if (event.keyCode === 13) {
-            // 	btnAdd.click();
-            // };
+        // inputBox.addEventListener("keyup", event => {
+        //     // if (event.keyCode === 13) {
+        //     // 	btnAdd.click();
+        //     // };
 
-        }); // this allows you to hit 'enter' on your keyboard to add a value, instead of having to click the 'add' button every time.
+        //}); // this allows you to hit 'enter' on your keyboard to add a value, instead of having to click the 'add' button every time.
 
         // function undo(){
         // 	txtCount--;
@@ -139,6 +141,7 @@
             if (data.length > 0) {
                 var pop = data.pop().trim();
                 undoData.push(pop);
+                console.log(data, undoData);
                 var result = '';
                 $.each(data, function(index, value) {
                     result += value;
@@ -153,6 +156,7 @@
             if (undoData.length > 0) {
                 var pop = undoData.pop().trim();
                 data.push(pop);
+                console.log(data, undoData);
                 var result = '';
                 $.each(data, function(index, value) {
                     result += value;
@@ -163,9 +167,10 @@
             }
         }
 
-        $(document).on('keyup', '#inputBox', function(e) {
+        $(document).on('keyup', '#inputBox', function() {
             var str = $(this).val().trim();
             data.push(str.charAt(str.length - 1));
+            console.log(data, undoData);
         });
 
         function handleOrientation(event) {
@@ -206,9 +211,9 @@
 
         window.addEventListener("deviceorientation", handleOrientation, true);
 
-        if (location.protocol != "https:") {
-            location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-        }
+        // if (location.protocol != "https:") {
+        //     location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+        // }
 
         function permission() {
             if (typeof(DeviceMotionEvent) !== "undefined" && typeof(DeviceMotionEvent.requestPermission) === "function") {
